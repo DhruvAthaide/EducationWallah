8df4b355dd525fe3bba614a95b8cba60093d3d1e
Sammmy22 <sambhavc225@outlook.com>
2023-04-18 13:22:30 +0530

    Small bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { UserContext, UserProvider } from "./contexts/user.context";
+import { UserProvider } from "./contexts/user.context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserContext>
+      <UserProvider>
         <App />
-      </UserContext>
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
