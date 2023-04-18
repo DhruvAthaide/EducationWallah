9c5bef472eeb588df6ead609bc95b6afbe9948ba
Sammmy22 <sambhavc225@outlook.com>
2023-04-18 17:12:04 +0530

    Added About Component

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useContext } from "react";
-import "./home.styles.css";
-import { UserContext } from "../../contexts/user.context";
-import { useNavigate } from "react-router-dom";
+// import React, { useEffect, useContext } from "react";
+// import "./home.styles.css";
+// import { UserContext } from "../../contexts/user.context";
+// import { useNavigate } from "react-router-dom";
 
-export default function Home() {
-  const { currentUser } = useContext(UserContext);
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (!currentUser) {
-      navigate("/");
-    }
-  }, []);
-  return <div>Home</div>;
-}
+// export default function Home() {
+//   const { currentUser } = useContext(UserContext);
+//   const navigate = useNavigate();
+//   useEffect(() => {
+//     if (!currentUser) {
+//       navigate("/");
+//     }
+//   }, []);
+//   return <div>Home</div>;
+// }
