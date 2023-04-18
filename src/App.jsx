34f0ef58e3ab581f14280ff78a661ea504bf9e8c
Sammmy22 <sambhavc225@outlook.com>
2023-04-18 18:36:54 +0530

    Completed Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Authentication from "./components/authentication/authentication.component";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/home/home.component";
+import Navbar from "./components/navbar/navbar.component";
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
       <Route path="/">
         <Route index element={<Authentication />} />
       </Route>
-      <Route path="home" element={<Home />} />
+      <Route path="home" element={<Navbar />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }
--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useContext } from "react";
 import { UserContext } from "../../contexts/user.context";
 import { useNavigate } from "react-router-dom";
+import "./home.styles.css";
 
 export default function Home() {
   const { currentUser } = useContext(UserContext);
@@ -11,5 +12,5 @@ export default function Home() {
     }
   }, []);
 
-  return <div>Home</div>;
+  return <div className="home-container">Home</div>;
 }
