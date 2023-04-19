3a427a3e12d9275a35423d7f8c37c604b576ef67
Dhruv Athaide <athaidedhruv@gmail.com>
2023-04-19 07:03:59 +0530

    restored auth

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -6,7 +6,8 @@ const Footer = () => {
   return (
     <>
       <section className='newletter'>
-        <div className='container flexSB'>
+        
+        <div className='container-nl flexSB'>
           <div className='left row'>
             <h1>Newsletter - Stay tune and get the latest update</h1>
             <span>Far far away, behind the word mountains</span>
